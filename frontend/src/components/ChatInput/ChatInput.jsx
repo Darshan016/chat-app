112a3b222446fee324efbade02b182cc3f5b7e4c
darshan panchal <panchaldarshan121@gmail.com>
2024-08-04 15:28:28 +0530

    css cleaning

--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -1,12 +1,14 @@
-import React, {Component} from "react";
-import './ChatInput.scss';
+import React, { Component } from "react";
+import "./ChatInput.scss";
 
-class ChatInput extends Component{
-    render(){
-        return (
-            <div className="ChatInput">
-                <input onKeyDown={this.props.send} placeholder="enter something" />
-            </div>
-        )
-    }
+class ChatInput extends Component {
+  render() {
+    return (
+      <div className="ChatInput">
+        <input onKeyDown={this.props.send} placeholder="enter something" />
+      </div>
+    );
+  }
 }
+
+export default ChatInput;
